dfd7dcf5c2859212158a9f6e8beec2f35de7870f
David <davidlemegon@hotmail.com>
2026-07-22 18:01:30 -0300

    first commit

---
+++ transaction-authorization-api/src/main/java/com/itau/transaction/TransactionAuthorizationApiApplication.java
@@ -0,0 +1,13 @@
+package com.itau.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionAuthorizationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionAuthorizationApiApplication.class, args);
+	}
+
+}
---
+++ transaction-authorization-api/src/test/java/com/itau/transaction/TransactionAuthorizationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionAuthorizationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
